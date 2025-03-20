0bffcea7ec9233f95368e6cdae0a03033c12f327
catuaba <catuaba@riseup.net>
2025-03-20 20:07:56 +0000

    Adicionando ovos para testar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,6 +28,9 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             e papas y maiz, ademas de palomitas?
           </li>
+          <li className="tracking-[-.01em]">
+            e ovo, e ovo
+          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
