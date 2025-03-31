5592648aee4d0ffee54a35d881d00a0dcd307615
Gustavo Galo <gg.gustavo.galo@gmail.com>
2025-03-30 22:36:32 -0300

    feat: add PUT route to edit product

---
+++ src/app/api/product/[product_id]/route.ts
@@ -0,0 +1,63 @@
+import { prisma } from "@/lib/prisma";
+import { Category } from "@prisma/client";
+
+type params = { params: Promise<{ product_id: string }> };
+export async function PUT(request: Request, { params }: params) {
+  const { product_id } = await params;
+  const body = await request.json();
+  const category = body.category;
+
+  if (request.headers.get("API_KEY") !== process.env.API_KEY) {
+    return new Response(JSON.stringify({ error: "Invalid API Key" }), {
+      status: 401,
+    });
+  }
+
+  const product = await prisma.product.findUnique({
+    where: { id: product_id },
+  });
+
+  if (!product) {
+    return new Response(JSON.stringify({ error: "Product not found" }), {
+      status: 404,
+    });
+  }
+
+  if (category) {
+    if (
+      category !== Category.AGRICOLA &&
+      category !== Category.ARTESANATO &&
+      category !== Category.PROCESSADO
+    ) {
+      return new Response(
+        JSON.stringify({
+          error: `Category must be one of "${Category.AGRICOLA}", "${Category.ARTESANATO}", "${Category.PROCESSADO}"`,
+        }),
+        { status: 400 }
+      );
+    }
+
+    product.category = category;
+  }
+
+  if (body.product_name) {
+    product.product_name = body.product_name;
+  }
+
+  if (body.description) {
+    product.description = body.description;
+  }
+
+  const editedProduct = await prisma.product.update({
+    data: product,
+    where: { id: product_id },
+  });
+
+  return new Response(
+    JSON.stringify({
+      message: "Product successfully edited",
+      data: editedProduct,
+    }),
+    { status: 201 }
+  );
+}
