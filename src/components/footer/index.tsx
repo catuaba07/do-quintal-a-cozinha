055ea5a2facfcecab5c1337556988f6d8c08fcc1
Gustavo Galo <gg.gustavo.galo@gmail.com>
2025-05-06 15:27:12 -0300

    feat: add footer component

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,96 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { cn } from "@/lib/utils";
+import { siteConfig } from "@/config/site";
+
+export function Footer() {
+  const pathname = usePathname();
+
+  return (
+    <footer className="w-full bg-purple-400/95 backdrop-blur supports-[backdrop-filter]:bg-purple-400/60">
+      <div className="container-wrapper">
+        <div className="container flex flex-col my-4 gap-2 md:gap-4">
+          <div className="flex justify-between lg:min-w-[1024px] m-auto flex-col lg:flex-row">
+            <Link href="/" className="mr-4 flex items-center gap-2 lg:mr-6">
+              <img src="/logo.png" alt="logo" height={64} width={64} />
+            </Link>
+            <div className="flex flex-col gap-2 my-auto">
+              <p className="font-bold">{siteConfig.name}</p>
+              <p className="font-bold">{siteConfig.mail}</p>
+            </div>
+            <div className="lg:grid grid-flow-col grid-rows-2 gap-x-8 flex flex-col">
+              <Link
+                href="/sobre"
+                className={cn(
+                  "transition-colors hover:text-foreground/80",
+                  pathname === "/sobre"
+                    ? "text-foreground"
+                    : "text-foreground/80"
+                )}
+              >
+                Sobre
+              </Link>
+              <Link
+                href="/nossa-historia"
+                className={cn(
+                  "transition-colors hover:text-foreground/80",
+                  pathname === "/sobre"
+                    ? "text-foreground"
+                    : "text-foreground/80"
+                )}
+              >
+                Nossa História
+              </Link>
+              <Link
+                href="/onde-estamos"
+                className={cn(
+                  "transition-colors hover:text-foreground/80",
+                  pathname === "/sobre"
+                    ? "text-foreground"
+                    : "text-foreground/80"
+                )}
+              >
+                Onde Estamos
+              </Link>
+              <Link
+                href="/nossa-producao"
+                className={cn(
+                  "transition-colors hover:text-foreground/80",
+                  pathname?.startsWith("/nossa-producao")
+                    ? "text-foreground"
+                    : "text-foreground/80"
+                )}
+              >
+                Nossa Produção
+              </Link>
+              <Link
+                href="/nossas-receitas"
+                className={cn(
+                  "transition-colors hover:text-foreground/80",
+                  pathname?.startsWith("/nossas-receitas")
+                    ? "text-foreground"
+                    : "text-foreground/80"
+                )}
+              >
+                Nossas Receitas
+              </Link>
+              <Link
+                href="/nosso-espaco"
+                className={cn(
+                  "transition-colors hover:text-foreground/80",
+                  pathname?.startsWith("/nosso-espaco")
+                    ? "text-foreground"
+                    : "text-foreground/80"
+                )}
+              >
+                Nosso Espaço
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
